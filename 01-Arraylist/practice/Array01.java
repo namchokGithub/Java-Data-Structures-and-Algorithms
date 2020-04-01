780a64de646d9eadcd1c551cdc78fbe6b9bd9883
Namchok <pech4751@gmail.com>
2020-04-01 15:28:25 +0700

    Add array01

---
+++ 01-Arraylist/practice/Array01.java
@@ -0,0 +1,28 @@
+package practice;
+/**
+ * Array
+ */
+public class Array01 {
+
+    public static void main(String[] args) {
+        int[] ary; 
+
+        // create a new integer array with 5 element
+        // ary now refers (points) to this new array
+        ary = new int[5];
+
+        // using the length attribute for check length
+        System.out.println("Length = " + ary.length);
+
+        // accessing individual array element
+        ary[0] = 100;
+        ary[1] = ary[0] - 20;
+        ary[2] = ary[1] / 2;
+
+        // print array
+        for (int i=0; i<ary.length; i++) {
+            System.out.println("ary["+ i +"] = " + ary[i]);
+        }
+
+    }
+}
